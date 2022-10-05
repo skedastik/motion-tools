a6b63f6f2e81f3dcdd61a7574255543eee2bc98b
Alaric Holloway <alaric.holloway@gmail.com>
2022-10-05 15:17:46 -0400

    Add `getSelectedPropertiesFrom` function

--- lib/MT.jsx
+++ lib/MT.jsx
@@ -1,44 +1,71 @@
-module.exports = {
-    // get an array of properties from a given layer
-    getPropertiesFrom: function (layer) {
-        var props = [];
+(function () {
+    var LogFactory = require('./LogFactory.jsx');
 
-        function _getAllProperties(o) {
-            for (var i = 1; i <= o.numProperties; i++) {
-                var p = o.property(i);
-                props.push(p);
-                _getAllProperties(p);
+    var log = new LogFactory('motion-tools.log');
+
+    module.exports = {
+        // get an array of properties from a given layer
+        getPropertiesFrom: function (layer) {
+            var props = [];
+
+            function _getAllProperties(o) {
+                for (var i = 1; i <= o.numProperties; i++) {
+                    var p = o.property(i);
+                    props.push(p);
+                    _getAllProperties(p);
+                }
             }
-        }
-        
-        _getAllProperties(layer);
+            
+            _getAllProperties(layer);
 
-        return props;
-    },
+            return props;
+        },
 
-    // get a sorted array of all distinct keyframe times from a given array of properties
-    getDistinctSortedKeyTimesFrom: function (properties) {
-        var keyTimes = [];
-        var distinctKeyTimes = {};
+        // get an array of selected properties from a given layer
+        getSelectedPropertiesFrom: function (layer) {
+            var props = [];
 
-        for (var i = 0; i < properties.length; i++) {
-            var prop = properties[i];
-            var numKeys = prop.numKeys;
-            if (numKeys !== undefined) {
-                for (var j = 1; j <= numKeys; j++) {
-                    var keyTime = prop.keyTime(j);
-                    if (!distinctKeyTimes[keyTime]) {
-                        distinctKeyTimes[keyTime] = true;
-                        keyTimes.push(keyTime);
+            function _getAllSelectedProperties(o) {
+                for (var i = 1; i <= o.numProperties; i++) {
+                    var p = o.property(i);
+
+                    if (p.selectedKeys && p.selectedKeys.length > 0) {
+                        props.push(p);
+                    }
+                    
+                    _getAllSelectedProperties(p);
+                }
+            }
+            
+            _getAllSelectedProperties(layer);
+
+            return props;
+        },
+
+        // get a sorted array of all distinct keyframe times from a given array of properties
+        getDistinctSortedKeyTimesFrom: function (properties) {
+            var keyTimes = [];
+            var distinctKeyTimes = {};
+
+            for (var i = 0; i < properties.length; i++) {
+                var prop = properties[i];
+                var numKeys = prop.numKeys;
+                if (numKeys !== undefined) {
+                    for (var j = 1; j <= numKeys; j++) {
+                        var keyTime = prop.keyTime(j);
+                        if (!distinctKeyTimes[keyTime]) {
+                            distinctKeyTimes[keyTime] = true;
+                            keyTimes.push(keyTime);
+                        }
                     }
                 }
             }
-        }
 
-        keyTimes.sort(function (a, b) {
-            return a - b;
-        });
-        
-        return keyTimes;
-    }
-};
+            keyTimes.sort(function (a, b) {
+                return a - b;
+            });
+            
+            return keyTimes;
+        }
+    };
+})();
